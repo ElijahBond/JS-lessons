e959a89295bd7667fe522ddd540fed7f53e5c461
ElijahBond <bondarefil@gmail.com>
2022-10-28 22:00:26 +0300

    thierd part

--- 2 films/script.js
+++ 2 films/script.js
@@ -24,3 +24,83 @@ personalMoviesDB.movies[latterFilm2] = score2
 
 
 console.log(personalMoviesDB)
+
+
+
+
+
+
+
+
+
+
+
+console.log( NaN || 2 || undefined ) ; // 2+
+
+console.log( NaN && 2 && undefined ); //  NaN+
+ 
+console.log( 1 && 2 && 3 ); // 3+
+ 
+console.log( !1 && 2 || !3 );  //  false+
+ 
+console.log( 25 || null && !3 );  // 25+
+ 
+console.log( NaN || null || !3 || undefined || 5); // 5+
+ 
+console.log( NaN || null && !3 && undefined || 5);  // 5+
+ 
+console.log( 5 === 5 && 3 > 1 || 5); // -
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+if (hamburger === 3 && cola || fries === 3 && nuggets) {  // (false || 2)
+   console.log('Done!')
+}
+
+// console.log( hamburger === 3 && cola || fries === 3 && nuggets )
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+}
+
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {  // (false || false)
+   console.log('Done!')
+}
+
+
+
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++){
+   
+    for (let j = 0; j < i; j++){
+        result += '*';
+    }
+
+    result += '\n';
+}
+
+console.log(result)
+
+
+
+
---
+++ 3 films-2/script.js
@@ -0,0 +1,48 @@
+let resultOfFilms = +prompt('How much films are you seen yet?');
+
+let personalMoviesDB = {
+    count: resultOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+
+
+quest: for (let i = 0; i < 2; i++){
+    let latterFilm = prompt('Latest film which you seen?');
+    let score = +prompt('How much stars you give it? / How much you assess it?');
+
+     if( latterFilm != null && score != null && latterFilm != '' && score != '' && latterFilm.length < 50 ){
+        personalMoviesDB.movies[latterFilm] = score
+     } else {
+        i--
+     }
+
+
+    
+}
+
+if (personalMoviesDB.count < 10){
+    console.log('WOW you seen too litle movie')
+} else if ( personalMoviesDB.count > 10 && personalMoviesDB.count < 30) {
+    console.log('you classic watcher')
+} else if ( personalMoviesDB.count > 30) {
+    console.log('you kinoman')
+} else {
+    console.log('error :(')
+}
+
+console.log(personalMoviesDB)
+
+
+
+
+// let num = 10;
+
+for (let i = 1; i < 2; i++){
+  var  num = 15;
+    console.log(i);
+};
+
+console.log(num)
